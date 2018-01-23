747b75f477055278cb0774519cd301de0f7dca4f
NavchetanKaur <nkaur@tulane.edu>
2018-01-23 13:38:26 -0600

    Script to run blast by biopython

---
+++ biopython_blast.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python
+
+import os
+import Bio
+import Bio.Align.Applications
+
+from Bio.Blast.Applications import NcbiblastpCommandline
+
+file = "ARC_sequence.fasta"
+database = "viral.1.protein.faa"
+
+blastp_cline = NcbiblastpCommandline(query=file, db=database, outfmt=5, out="blast_output.xml")
+print(blastp_cline)
+stdout, stderr = blastp_cline()
