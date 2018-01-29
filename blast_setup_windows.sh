31f1d4de12c043349a66a1c71a7a576179da73d7
Loren Gragert <lgragert@tulane.edu>
2018-01-29 10:35:09 -0600

    Add BLAST binaries to path

--- blast_setup_windows.sh
+++ blast_setup_windows.sh
@@ -10,5 +10,8 @@ cd blastdb
 curl -O ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.faa.gz
 gunzip *.gz
 
+# Add BLAST binaries to path
+echo 'export PATH="/cygdrive/c/tools/cygwin/ncbi-blast-2.7.1+/bin:$PATH"' >> ~/.profile
+
 makeblastdb -in viral.1.protein.faa -dbtype prot
 
