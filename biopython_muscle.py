0b870c012ad69b118f7059b0f1fc6c17ec97a459
NavchetanKaur <nkaur@tulane.edu>
2018-01-31 00:13:43 -0600

    MUSCLE using biopython

---
+++ biopython_muscle.py
@@ -0,0 +1,14 @@
+import os
+import Bio
+
+from Bio.Align.Applications import MuscleCommandline
+
+mus = "/Users/navchetankaur/Documents/bioinfo_course/sequence-alignment/muscle"
+
+in_file = "MSA_sequences_file.fasta"
+
+out_file = "MSA_muscle_output.txt"
+
+cline = MuscleCommandline(mus, input=in_file, out=out_file)
+print(cline)
+stdout, stderr = cline() 
