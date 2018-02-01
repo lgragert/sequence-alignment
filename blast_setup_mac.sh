2242e182e8e78a997b5d0d368efd3eb78868297a
lgragert <lgragert@tulane.edu>
2018-02-01 15:20:07 -0600

    Move executables to /usr/local/ and added comments to BLAST install scripts

--- blast_setup_mac.sh
+++ blast_setup_mac.sh
@@ -1,21 +1,35 @@
 #!bin/bash
 
+# Make sure you've downloaded all scripts from Canvas to a working directory
+# before running this script.
+# This script can be run with "bash blast_setup_mac.sh" in terminal
+# Can also run this by setting as an executable with the command
+# "chmod +x blast_setup_mac.sh" followed by "./blast_setup_mac.sh"
+
+# Downloads BLAST Mac install - compressed using tar and gzip
 curl -O ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.7.1+-x64-macosx.tar.gz
 
+# Uncompresses BLAST distribution
 tar xzf ncbi-*.tar.gz
 cd ncbi-blast-2.7.1+
 
+# copies BLAST binaries to /usr/local/ and add directory to PATH
 cd bin
-mkdir /usr/local/ncbi/blast/bin/
-chmod 777 /usr/local/ncbi/blast/bin/*
+mkdir -p /usr/local/ncbi/blast/bin/
+chmod 777 /usr/local/ncbi/blast/bin/* # make give error if no file there already
 cp * /usr/local/ncbi/blast/bin/
-echo 'export PATH="/usr/local/ncbi/blast/bin:$PATH"' >> ~./bash_profile
+PATH=/usr/local/ncbi/blast/bin:$PATH # add path for session
+echo 'export PATH="/usr/local/ncbi/blast/bin:$PATH"' >> ~/.bash_profile
 cd ..
 
+# Make a directory to store the BLAST database
 mkdir blastdb
 cd blastdb
 
+# Downloads and uncompressed viral protein FASTA file - compressed with gzip
 curl -O ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.faa.gz
 gunzip *.gz
 
+# Creates BLAST database from viral protein FASTA file
+# Should make .phr, .pin, and .psq files if successful
 makeblastdb -in viral.1.protein.faa -dbtype prot
--- blast_setup_windows.sh
+++ blast_setup_windows.sh
@@ -1,17 +1,35 @@
+#!bin/bash
 
+# Make sure you've downloaded all scripts from Canvas to a working directory
+# before running this script.
+# This script can be run with "bash blast_setup_windows.sh" in Cygwin
+# Can also run this by setting as an executable with the command
+# "chmod +x blast_setup_windows.sh" followed by "./blast_setup_windows.sh"
 
+# Downloads BLAST Windows install - compressed using tar and gzip
 curl -O ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.7.1+-x64-win64.tar.gz
 
+# Uncompresses BLAST distribution
 tar xzf ncbi-*.tar.gz
 cd ncbi-blast-2.7.1+
+
+# copies BLAST binaries to /usr/local/ and add directory to PATH
+cd bin
+mkdir -p /usr/local/ncbi/blast/bin/
+chmod 777 /usr/local/ncbi/blast/bin/* # make give error if no file there already
+cp * /usr/local/ncbi/blast/bin/
+PATH=/usr/local/ncbi/blast/bin:$PATH # add path for session
+echo 'export PATH="/usr/local/ncbi/blast/bin:$PATH"' >> ~/.bash_profile
+cd ..
+
+# Make a directory to store the BLAST database
 mkdir blastdb
 cd blastdb
 
+# Downloads and uncompressed viral protein FASTA file - compressed with gzip
 curl -O ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.faa.gz
 gunzip *.gz
 
-# Add BLAST binaries to path
-echo 'export PATH="/cygdrive/c/tools/cygwin/ncbi-blast-2.7.1+/bin:$PATH"' >> ~/.profile
-
+# Creates BLAST database from viral protein FASTA file
+# Should make .phr, .pin, and .psq files if successful
 makeblastdb -in viral.1.protein.faa -dbtype prot
-
