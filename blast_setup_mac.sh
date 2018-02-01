c9a7190efb4cd1761375ba2b8e0d0cadabbed393
lgragert <lgragert@tulane.edu>
2018-02-01 12:14:01 -0600

    blast_setup_mac.sh

--- blast_setup_mac.sh
+++ blast_setup_mac.sh
@@ -1,12 +1,15 @@
-
-
-
-
+#!bin/bash
 
 curl -O ftp://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.7.1+-x64-macosx.tar.gz
 
 tar xzf ncbi-*.tar.gz
 cd ncbi-blast-2.7.1+
+
+cd bin
+mkdir /usr/local/ncbi/blast/bin/
+cp * /usr/local/ncbi/blast/bin/
+echo 'export PATH="/usr/local/ncbi/blast/bin:$PATH"' >> ~./bash_profile
+cd ..
 
 mkdir blastdb
 cd blastdb
@@ -15,4 +18,3 @@ curl -O ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.protein.faa.gz
 gunzip *.gz
 
 makeblastdb -in viral.1.protein.faa -dbtype prot
-
