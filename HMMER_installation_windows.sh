f2a4c7d549840526c3327e552851539ca21ae9d6
NavchetanKaur <nkaur@tulane.edu>
2018-02-02 11:35:38 -0600

    HMMER installation on MAC and windows

---
+++ HMMER_installation_windows.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+
+# Make sure you are in working directory 
+
+
+#Downloads HMMER
+curl -OL http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-cygwin64.tar.gz
+
+#unpack 
+tar xvf hmmer-3.1b2-cygwin64.tar.gz
+
+cd hmmer-3.1b2-macosx-intel
+# Compile the software with configure and make
+./configure
+
+make
+
+### runs a test suite
+make check
+
+### installs the program in /usr/local/bin
+make install
