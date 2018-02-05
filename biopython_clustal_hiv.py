bfa6684abbdf146c4ce1081c4c4d8ed2a394fc81
lgragert <lgragert@tulane.edu>
2018-02-05 14:30:54 -0600

    Added comments about calling CLUSTALW to align HIV sequences

--- biopython_clustal_hiv.py
+++ biopython_clustal_hiv.py
@@ -5,10 +5,8 @@ import Bio
 
 from Bio.Align.Applications import ClustalwCommandline
 
+# Call CLUSTALW using BioPython wrapper to align HIV proteome sequences
 #cwline = ClustalwCommandline("/Users/navchetankaur/Documents/bioinfo_course/sequence-alignment/clustalw2", infile="MSA_sequences_file.fasta")
 cwline = ClustalwCommandline("clustalw2", infile="hiv-proteome.fasta")
 print(cwline)
 stdout, stderr = cwline()
-
-
-
