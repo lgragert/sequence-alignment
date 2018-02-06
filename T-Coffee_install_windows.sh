7797d6d1a36b84b4415bdf12ee7ef4734c7260a8
lgragert <lgragert@tulane.edu>
2018-02-06 14:34:01 -0600

    Added CPAN XML install line to T-Coffee script

--- T-Coffee_install_windows.sh
+++ T-Coffee_install_windows.sh
@@ -3,13 +3,16 @@
 # Install fort77 compiler
 apt-cyg install fort77
 
+# Install XML::Simple CPAN Module for Perl (this will take a while)
+cpan install XML::Simple
+
 # Download T-Coffee Source Distribution using curl
 curl -OL http://www.tcoffee.org/Packages/Stable/Latest/T-COFFEE_distribution_Version_11.00.8cbe486.tar.gz
 
 # Extract T-Coffee source code archive
 tar xvf T-COFFEE_distribution_Version_11.00.8cbe486.tar.gz
 
-# Compile Install T-Coffee (this will take a while)
+# Compile and Install T-Coffee (this will take a while - even longer!)
 cd T-COFFEE_distribution_Version_11.00.8cbe486/
 ./install all
 cd ..
