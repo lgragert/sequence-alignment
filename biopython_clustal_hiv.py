d452bedd3c005cce497e908bd2fb722f7a899be2
Loren Gragert <lgragert@tulane.edu>
2019-01-30 11:41:13 -0600

    Removed old path

--- biopython_clustal_hiv.py
+++ biopython_clustal_hiv.py
@@ -6,7 +6,6 @@ import Bio
 from Bio.Align.Applications import ClustalwCommandline
 
 # Call CLUSTALW using BioPython wrapper to align HIV proteome sequences
-#cwline = ClustalwCommandline("/Users/navchetankaur/Documents/bioinfo_course/sequence-alignment/clustalw2", infile="MSA_sequences_file.fasta")
 cwline = ClustalwCommandline("clustalw2", infile="hiv-proteome.fasta")
 print(cwline)
 stdout, stderr = cwline()
--- biopython_clustalw.py
+++ biopython_clustalw.py
@@ -5,7 +5,6 @@ import Bio
 
 from Bio.Align.Applications import ClustalwCommandline
 
-#cwline = ClustalwCommandline("/Users/navchetankaur/Documents/bioinfo_course/sequence-alignment/clustalw2", infile="MSA_sequences_file.fasta")
 cwline = ClustalwCommandline("clustalw2", infile="MSA_sequences_file.fasta")
 print(cwline)
 stdout, stderr = cwline()
