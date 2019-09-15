40aef4cd77d3a3dd8233c474b9ad9a414bdc0481
Loren Gragert <lgragert@tulane.edu>
2019-09-15 14:58:36 -0500

    Fall 2019 Mac Update

--- MSA_setup_mac.sh
+++ MSA_setup_mac.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-#################################################################################################################################################################
+##############################################################################
+# Multiple Sequence Alignment - In-Class Lab
 # 
-# Lecture 5: Multiple sequence alignment
-# Instructor: Dr Loren Gragert, Assistant Professor, Department of Pathology and Laboratory Medicine, Tulane School of Medicine.
-# 
-###############################################################################################################################################################
+# Instructor: Dr Loren Gragert
+# Assistant Professor
+# Department of Pathology and Laboratory Medicine
+# Tulane School of Medicine
 
-###############################################################################################################################################################
-# INSTRUCTIONS FOR RUNNING CLUSTAL W WEB TOOL
-###############################################################################################################################################################
+###############################################################################
+# INSTRUCTIONS FOR RUNNING CLUSTALW WEB TOOL
+###############################################################################
+# 1) Open the CLUSTAL W homepage at http://www.genome.jp/tools-bin/clustalw
+# 2) Copy and paste the FASTA sequences from MSA_sequences_file.fasta (provided) 
+#       or upload the file
+# 3) Output format can remain as default
+# 4) For pairwise alignment select SLOW/ACCURATE
+# 5) Execute Multiple Alignment
+# 6) From the tree menu try executing with Neighbor-joining/UPGMA formats
 
-# 	1) Open the CLUSTAL W homepage at http://www.genome.jp/tools-bin/clustalw
-# 	2) Copy and paste the fasta sequences from MSA_sequences_file.fasta (provided) or upload the file
-# 	3) Output format can remain as default
-# 	4) For pairwise alignment select SLOW/ACCURATE
-# 	5) Execute Multiple Alignment
-# 	6) From the tree menu try executing with Neighbor-joining/UPGMA formats
 
-################################################################################################################################################################
-# 
+###############################################################################
 # INSTRUCTIONS FOR MULTIPLE SEQUENCE ALIGNMENTS WITH BIOPYTHON
-# 
-###############################################################################################################################################################
+###############################################################################
 # Download and Install CLUSTAL W and MUSCLE on your system
-###############################################################################################################################################################
+###############################################################################
 
-# From Unix terminal change directory to the current directory where you have all the files
+# From Unix terminal change directory to the current directory where you have 
+# all the files
 
 # CLUSTALW
 
@@ -43,19 +44,18 @@ mv muscle3.8.31_i86darwin64 muscle
 cp muscle /usr/local/bin
 
 #############################################################################################################################################################
-# T-Coffee installation on Mac OSX
+# T-COFFEE installation on Mac =OS
 #############################################################################################################################################################
 
-# Download and run installer
-curl -OL http://www.tcoffee.org/Packages/Beta/Latest/T-COFFEE_distribution_Version_12.00.451ed9b.tar.gz
-
-tar -xvfz T-COFFEE_distribution_Version_12.00.451ed9b.tar.gz
-
-cd T-COFFEE_distribution_Version_12.00.451ed9b
+# Downloads and run installer
+curl -OL http://www.tcoffee.org/Packages/Beta/Latest/T-COFFEE_distribution_Version_13.40.5.a96e565.tar.gz
+tar xvfz T-COFFEE_distribution_Version_13.40.5.a96e565.tar.gz
+cd T-COFFEE_distribution_Version_13.40.5.a96e565
 
 # you may have to enter passwords and answer questions
 
 ./install tcoffee
+./install all
 
 # install new clang compiler does not work
 
@@ -74,7 +74,9 @@ cd T-COFFEE_distribution_Version_12.00.451ed9b
 # add t_coffee to path and Bash config file
 
 export PATH=~/.t_coffee/plugins/macosx:$PATH
+export PLUGINS_4_TCOFFEE=~/.t_coffee/plugins/macosx:
 echo 'export PATH="~/.t_coffee/plugins/macosx:$PATH"' >> ~/.bash_profile
+echo 'export PLUGINS_4_TCOFFEE="~/.t_coffee/plugins/macosx:"' >> ~/.bash_profile
 source ~/.bash_profile
 
 # go back to working directory and check version
@@ -82,11 +84,11 @@ cd ..
 t_coffee -version
 
 
-#############################################################################################################################################################
+###############################################################################
 # Running CLUSTAL W and MUSCLE and T-COFFEE with BIOPYTHON
-#############################################################################################################################################################
+###############################################################################
 
-# To run CLUSTAL W and MUSCLE using biopython, run the commands from unix shell
+# To run CLUSTAL W and MUSCLE using biopython, run the commands from Bash shell
 
 python3 biopython_clustalw.py
 python3 biopython_muscle.py
--- T-Coffee_install_windows.sh
+++ T-Coffee_install_windows.sh
@@ -1,4 +1,5 @@
-# Install Script for T-Coffee for Windows
+# Install Script for T-Coffee for Windows - 
+# T-Coffee does not support Windows - this is a work in progress
 
 # Install Cygwin compilers and devel tools
 apt-cyg install fort77
@@ -21,12 +22,13 @@ cpan install SOAP::Lite
 cpan -fi SOAP::Lite
 
 # Download T-Coffee Source Distribution using curl
-curl -OL http://www.tcoffee.org/Packages/Stable/Latest/T-COFFEE_distribution_Version_11.00.8cbe486.tar.gz
+curl -OL http://www.tcoffee.org/Packages/Stable/Latest/T-COFFEE_distribution_Version_13.39.0.d675aed.tar.gz
 
 # Extract T-Coffee source code archive
-tar xvf T-COFFEE_distribution_Version_11.00.8cbe486.tar.gz
+tar xvf T-COFFEE_distribution_Version_13.39.0.d675aed.tar.gz
 
 # Compile and Install T-Coffee (this will take a while - even longer!)
-cd T-COFFEE_distribution_Version_11.00.8cbe486/
+cd T-COFFEE_distribution_Version_13.39.0.d675aed
+./install tcoffee
 ./install all
 cd ..
