12cefda1f5a97be948dea58fb8d7250413bc7d74
Loren Gragert <lgragert@tulane.edu>
2019-09-16 15:50:36 -0500

    T-Coffee setup split because of time

--- MSA_setup_windows.sh
+++ MSA_setup_windows.sh
@@ -10,9 +10,9 @@
 ###############################################################################
 
 ###############################################################################
-# INSTRUCTIONS FOR RUNNING CLUSTAL W WEB TOOL
+# INSTRUCTIONS FOR RUNNING CLUSTALW WEB TOOL
 ###############################################################################
-#	1) Open the CLUSTAL W homepage at http://www.genome.jp/tools-bin/clustalw
+#	1) Open the CLUSTALW homepage at http://www.genome.jp/tools-bin/clustalw
 # 	2) Copy and paste the fasta sequences from MSA_sequences_file.fasta 
 # 		(provided) or upload the file
 # 	3) Output format can remain as default
@@ -23,67 +23,28 @@
 ################################################################################
 # INSTRUCTIONS FOR MULTIPLE SEQUENCE ALIGNMENTS WITH BIOPYTHON
 ################################################################################
-# Download CLUSTAL W and MUSCLE and T-COFFEE on your system
+# Download CLUSTALW and MUSCLE on your system
 ################################################################################
 
-#  CLUSTALW
-#  From Cygwin terminal, change directory to the current directory where you have 
-#  all the files
+# CLUSTALW installation
 
 curl -O http://www.clustal.org/download/current/clustalw-2.1-win.msi
-run msiexec //a clustalw-2.1-win.msi /qn TARGETDIR=C:\\
+run msiexec /a clustalw-2.1-win.msi /qn TARGETDIR=C:\\
 cp '/cygdrive/c/clustalw2.exe' /usr/local/bin
 
-# MUSCLE Installation - Cygwin version does not work with Chocolatey install
+# MUSCLE Installation - Uses regular Win32 version
+# Cygwin version does not work with Chocolatey install
 
 curl -OL https://www.drive5.com/muscle/downloads3.8.31/muscle3.8.31_i86win32.exe
 chmod +x muscle3.8.31_i86win32.exe
 mv muscle3.8.31_i86win32.exe muscle.exe
 cp muscle.exe /usr/local/bin
 
-# T-Coffee for Windows - not officially supported
-
-# Install Cygwin compilers and devel tools
-choco install strawberryperl
-apt-cyg install fort77
-apt-cyg install libcrypt-devel
-apt-cyg install gcc-g++
-apt-cyg install make
-apt-cyg install openssl-devel
-apt-cyg install libexpat-devel
-apt-cyg install httpd-devel
-apt-cyg install libgdbm-devel
-apt-cyg install gdbm
-apt-cyg install libncurses-devel
-apt-cyg install libreadline-devel
-apt-cyg install libnsl-devel
-
-
-# Download T-Coffee Source Distribution using curl
-curl -OL http://www.tcoffee.org/Packages/Stable/Latest/T-COFFEE_distribution_Version_13.39.0.d675aed.tar.gz
-
-# Extract T-Coffee source code archive
-tar xvf T-COFFEE_distribution_Version_13.39.0.d675aed.tar.gz
-
-# Compile and Install T-Coffee and select plugins
-cd T-COFFEE_distribution_Version_13.39.0.d675aed
-./install tcoffee
-./install all
-
-cd ..
-export PATH=~/.t_coffee/bin/windows:$PATH
-export PLUGINS_4_TCOFFEE=~/.t_coffee/plugins/windows:
-echo 'export PATH="~/.t_coffee/bin/windows:$PATH"' >> ~/.bash_profile
-echo 'export PLUGINS_4_TCOFFEE="~/.t_coffee/plugins/windows:"' >> ~/.bash_profile
-source ~/.bash_profile
-cp ~/.t_coffee/bin/windows/t_coffee.exe /usr/local/bin/
-
 ###############################################################################
-# Running CLUSTAL W and MUSCLE with BioPython
+# Running CLUSTALW and MUSCLE with BioPython
 ###############################################################################
-# To run CLUSTAL W and MUSCLE using BioPython, 
+# To run CLUSTALW and MUSCLE using BioPython, 
 # run these commands from Bash shell
 
 python biopython_clustalw.py
 python biopython_muscle.py
-python biopython_tcoffee.py
---
+++ T-Coffee_setup_windows.sh
@@ -0,0 +1,72 @@
+# T-Coffee for Windows - not officially supported
+
+# Install Cygwin compilers and devel tools
+choco install strawberryperl
+apt-cyg install fort77
+apt-cyg install libcrypt-devel
+apt-cyg install gcc-g++
+apt-cyg install make
+apt-cyg install openssl-devel
+apt-cyg install libexpat-devel
+apt-cyg install apache2
+apt-cyg install apache2-devel
+apt-cyg install httpd-devel
+apt-cyg install httpd-tools
+apt-cyg install httpd-mod_ldap
+apt-cyg install httpd-mod_lua
+apt-cyg install httpd-mod_proxy_html
+apt-cyg install libgdbm-devel
+apt-cyg install gdbm
+apt-cyg install libncurses-devel
+apt-cyg install libreadline-devel
+apt-cyg install libnsl-devel
+
+# Install Perl modules needed for T-Coffee compile
+# This will take a while
+cpan -fi YAML
+cpan -fi inc::latest
+cpan -fi PAR
+cpan -fi Inline
+cpan -fi Module::Build::Tiny
+cpan -fi Dist::Zilla
+cpan -fi Dist::Milla
+cpan -fi Minilla
+cpan -fi App::ModuleBuildTiny
+cpan -fi Software::License
+cpan -fi PL_files
+cpan -fi File::ShareDir
+cpan -fi Module::Signature
+cpan -fi CPANPLUS
+cpan -fi Devel::AssertOS::Unix
+cpan -fi Module::Build
+cpan -fi XML::SemanticDiff
+cpan -fi SOAP::Transport::TCP
+cpan -fi XMLRPC::Lite
+cpan -fi XML::Parser
+cpan -fi XML::Parser::Lite
+cpan -fi XML::Simple
+
+# These installs seem to require some Apache configuration - Hit Ctrl-C to stop
+cpan -fi mod_perl2
+cpan -fi SOAP::Lite
+
+# Download T-Coffee Source Distribution using curl
+curl -OL http://www.tcoffee.org/Packages/Stable/Latest/T-COFFEE_distribution_Version_13.39.0.d675aed.tar.gz
+
+# Extract T-Coffee source code archive
+tar xvf T-COFFEE_distribution_Version_13.39.0.d675aed.tar.gz
+
+# Compile and Install T-Coffee and select plugins
+cd T-COFFEE_distribution_Version_13.39.0.d675aed
+./install tcoffee
+./install all
+
+cd ..
+export PATH=~/.t_coffee/bin/windows:$PATH
+export PLUGINS_4_TCOFFEE=~/.t_coffee/plugins/windows:
+echo 'export PATH="~/.t_coffee/bin/windows:$PATH"' >> ~/.bash_profile
+echo 'export PLUGINS_4_TCOFFEE="~/.t_coffee/plugins/windows:"' >> ~/.bash_profile
+source ~/.bash_profile
+cp ~/.t_coffee/bin/windows/t_coffee.exe /usr/local/bin/
+
+python biopython_tcoffee.py
