68bd86fdef58521236a59fe6c2d44994ad637d16
Loren Gragert <lgragert@tulane.edu>
2019-09-16 20:58:51 -0500

    Added slicing and sequence selection to AlignIO Biopython Object

--- biopython_clustalw.py
+++ biopython_clustalw.py
@@ -9,13 +9,24 @@ cwline = ClustalwCommandline("clustalw2", infile="MSA_sequences_file.fasta")
 print(cwline)
 stdout, stderr = cwline()
 
-
-
 from Bio import AlignIO
 align = AlignIO.read("MSA_sequences_file.aln", "clustal")
 print(align)
 
+# Alignment objects can be manipulated
 
+# slice alignment
+align_slice = align[:, 400:420]
+print ("Slice of alignment from position 400 to 420\n")
+print(align_slice)
+
+# Make Phylogenetic Tree
 from Bio import Phylo
 tree = Phylo.read("MSA_sequences_file.dnd", "newick")
 Phylo.draw_ascii(tree)
+
+# select the 3 sequences in the top branch of phylo tree
+
+align_branch = align_slice[0:3]
+print ("Top branch - 3 sequences from position 400 to 420\n")
+print(align_branch)
